bec551f9db5a515bb51c4d3866f3f49374b9c1aa
Cleiton Silva <cleiton_.98@hotmail.com>
2020-06-08 22:07:17 -0300

    correcao @BeforeEach mockito para junit 5

--- src/test/java/com/creitu/minhasFinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/creitu/minhasFinancas/service/UsuarioServiceTest.java
@@ -3,32 +3,34 @@ package com.creitu.minhasFinancas.service;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.creitu.minhasFinancas.exception.RegraNegocioException;
-import com.creitu.minhasFinancas.model.entity.Usuario;
 import com.creitu.minhasFinancas.model.repository.UsuarioRepository;
+import com.creitu.minhasFinancas.service.impl.UsuarioServiceImpl;
 
-@SpringBootTest
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 public class UsuarioServiceTest {
 	
-	@Autowired
 	UsuarioService usuarioService;
-	
-	@Autowired
 	UsuarioRepository usuarioRepository;
+	
+	@BeforeEach
+	public void setUp() {
+		usuarioRepository = Mockito.mock(UsuarioRepository.class);
+		usuarioService = new UsuarioServiceImpl(usuarioRepository);
+	}
 
 	@Test
 	public void deveValidarEmail() {
 		//cenario
-		usuarioRepository.deleteAll();
+		Mockito.when(usuarioRepository.existsByEmail(Mockito.anyString())).thenReturn(false);
 
 		//acao
 		assertDoesNotThrow(()-> usuarioService.validarEmail("email@email.com"));
@@ -37,8 +39,7 @@ public class UsuarioServiceTest {
 	@Test
 	public void deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado() {
 		//cenario
-		Usuario usuario = Usuario.builder().nome("usuario").email("email@email.com").build();
-		usuarioRepository.save(usuario);
+		Mockito.when(usuarioRepository.existsByEmail(Mockito.anyString())).thenReturn(true);
 
 		//acao
 		Assertions.assertThrows(RegraNegocioException.class, () -> {
